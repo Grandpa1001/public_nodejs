e8e20edfc4508a8e632e58891ee33c79c025ac99
Kamil <kamil.bandzwolek@gmail.com>
2022-09-21 20:36:53 +0200

    v007
    Portfolio panel. Add 3 project. Graphics, title, description.

--- src/components/home/projects.jsx
+++ src/components/home/projects.jsx
@@ -4,93 +4,94 @@ import Card from '@material-ui/core/Card'
 import CardActions from '@material-ui/core/CardActions'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
-import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
 import rpg1 from 'assets/images/rpg1.webp'
 import kolektyw from 'assets/images/kolektyw.webp'
-import mugOfWar from 'assets/images/mugOfWar.webp'
+import mgrGracz_portfolio from 'assets/images/mgrGracz_portfol.webp'
+import gj from 'assets/images/gj.webp'
 import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 import img1 from 'assets/images/shape3.webp'
-import moonFlappyBird1 from 'assets/images/moonFlappyBird.webp'
-import { BgImg3, Header2 } from 'styles/style'
+import flappyBird from 'assets/images/FlappyBird.webp'
+import {Header2 } from 'styles/style'
 import styled, {keyframes} from 'styled-components'
+
 
 
 const Projects = () => {
 
 
   return (
+    <Container>
     <Div>
+      <Header2>Moje projekty</Header2>
    <Slider>
       <SliderTrack>
+      <Slide>
+        <Image title={`MoonFlappyBird`} src={flappyBird} alt="ig"/>
+        <ImageOverlay>
+        <ImageTitle>Moon Flappy Bird</ImageTitle>
+        <ImageDescription>Gra oparta o klasyczny</ImageDescription>
+        <ImageDescription>styl flappybird w tematyce web3.</ImageDescription>
+        <ImageLink href="https://play.unity.com/mg/other/moonflappybird" target="_blank">Zagraj</ImageLink>
+        </ImageOverlay>
+        </Slide>
         <Slide>
-        <Image title={`GitHub Grandpa1001`} src={rpg1} alt="ig"/>
+        <Image title={`GitHub Grandpa1001`} src={mgrGracz_portfolio} alt="ig"/>
+        <ImageOverlay>
+        <ImageTitle>Strona główna</ImageTitle>
+        <ImageDescription>Projekt własny portfolio.</ImageDescription>
+        <ImageLink href="https://github.com/Grandpa1001/public_nodejs" target="_blank">Link</ImageLink> 
+        </ImageOverlay>
+        </Slide>
+        <Slide>
+        <Image title={`Kolektyw`} src={kolektyw} alt="ig"/>
+        <ImageOverlay>
+        <ImageTitle>NFT Polska Collective</ImageTitle>
+        <ImageDescription>Wizja metaversu i kolektywu w nim umieszczonego. Wygrana praca konkursowa na "zdefiniuj kolektyw" dla NFT Polska.</ImageDescription>
+        <ImageLink href="https://drive.google.com/drive/u/0/folders/1-2yB60eHijGYJa4U7gMTdoAVsDfNIs2S" target="_blank">Pobierz</ImageLink> 
+        <ImageLink href="https://twitter.com/MgrGracz/status/1565102856730038272?s=20&t=03TLQ8-nqShxZlwY-d4BSw" target="_blank">Twitter</ImageLink> 
+        </ImageOverlay>
+        </Slide>
+  
+        <Slide>
+        <Image title={`MoonFlappyBird`} src={flappyBird} alt="ig"/>
+        <ImageOverlay>
+        <ImageTitle>Moon Flappy Bird</ImageTitle>
+        <ImageDescription>Gra oparta o klasyczny</ImageDescription>
+        <ImageDescription>styl flappybird w tematyce web3.</ImageDescription>
+        <ImageLink href="https://play.unity.com/mg/other/moonflappybird" target="_blank">Zagraj</ImageLink>
+        </ImageOverlay>
+        </Slide>
+        <Slide>
+        <Image title={`GitHub Grandpa1001`} src={mgrGracz_portfolio} alt="ig"/>
+        <ImageOverlay>
+        <ImageTitle>Strona główna</ImageTitle>
+        <ImageDescription>Projekt własny portfolio.</ImageDescription>
+        <ImageLink href="https://github.com/Grandpa1001" target="_blank">Link</ImageLink>
+        </ImageOverlay>
         </Slide>
         <Slide>
         <Image title={`GitHub Grandpa1001`} src={kolektyw} alt="ig"/>
-        </Slide>
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={rpg1} alt="ig"/>
-        </Slide>
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={mugOfWar} alt="ig"/>
-        </Slide>
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={moonFlappyBird1} alt="ig"/>
-        </Slide>
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={kolektyw} alt="ig"/>
-        </Slide>
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={rpg1} alt="ig"/>
-        </Slide>
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={mugOfWar} alt="ig"/>
-        </Slide>
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={moonFlappyBird1} alt="ig"/>
-        </Slide>
-
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={rpg1} alt="ig"/>
-        </Slide>
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={kolektyw} alt="ig"/>
-        </Slide>
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={rpg1} alt="ig"/>
-        </Slide>
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={mugOfWar} alt="ig"/>
-        </Slide>
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={moonFlappyBird1} alt="ig"/>
-        </Slide>
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={kolektyw} alt="ig"/>
-        </Slide>
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={rpg1} alt="ig"/>
-        </Slide>
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={mugOfWar} alt="ig"/>
-        </Slide>
-        <Slide>
-        <Image title={`GitHub Grandpa1001`} src={moonFlappyBird1} alt="ig"/>
+        <ImageOverlay>
+        <ImageTitle>NFT Polska Collective</ImageTitle>
+        <ImageDescription>Wizja metaversu i kolektywu w nim umieszczonego. Wygrana praca konkursowa na "zdefiniuj kolektyw" dla NFT Polska.</ImageDescription>
+        <ImageLink>LINK</ImageLink>
+        </ImageOverlay>
         </Slide>
 
 
       </SliderTrack>
    </Slider>
    </Div>
+   </Container>
   )
 
 }
 
 const Div = styled.div`
-  height: 300px;
+  height: 600px;
   margin-left:-40px;
   margin-right:-40px;
   @media only screen and (min-width: 100px) and (max-width: 320px) {
@@ -103,9 +104,22 @@ const Div = styled.div`
   }
 `
 
+const DivHeader = styled.div`
+  height: 200px;
+
+  @media only screen and (min-width: 100px) and (max-width: 320px) {
+
+  }
+  @media only screen and (min-width: 321px) and (max-width: 600px) {
+
+  }
+  @media only screen and (min-width: 601px) and (max-width: 1024px) {
+  }
+`
+
 const Slider = styled.div`
  
-  height: 260px;
+  height: 400px;
   margin: auto;
   position: relative;
   width: 100%;
@@ -120,7 +134,7 @@ const Slider = styled.div`
     height: 100%;
     position: absolute;
     width:20%;
-    z-index: 2;
+    z-index: 1;
   }
   &:before {
     right:0;
@@ -132,11 +146,6 @@ const Slider = styled.div`
     top: 0;
     transform: rotateZ(180deg);
   }
-
-
-
-
-
 
 
   @media only screen and (min-width: 100px) and (max-width: 320px) {
@@ -151,16 +160,15 @@ const Slider = styled.div`
 
 const spin = keyframes`
   0% { transform: translateX(0); }
-  100% { transform: translateX(calc(-250px *9));
+  100% { transform: translateX(calc(-300px *3));
   }
 
 `;
 
 
-
 const SliderTrack = styled.div`
   display: flex;
-  width: calc(250px * 18);
+  width: calc(300px * 18);
   animation: ${spin} 40s linear infinite;
 
 
@@ -180,12 +188,58 @@ const SliderTrack = styled.div`
 `;
 
 const Slide = styled.div`
-  height: 200px;
-  width: 250px;
+  height: 400px;
+  width: 300px;
   display: flex;
   align-tems: center;
   padding: 15px;
-  perspective: 100px;
+  perspective: 80px;
+  z-index: 1;
+ 
+  @media only screen and (min-width: 100px) and (max-width: 320px) {
+
+  }
+  @media only screen and (min-width: 321px) and (max-width: 600px) {
+   
+  }
+  @media only screen and (min-width: 601px) and (max-width: 1024px) {
+  }
+`;
+
+const ImageOverlay = styled.div`
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.6);
+  color: #ffffff;
+  display: flex;
+  font-family: 'Montserrat';
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+  transition: opacity 0.55s;
+  z-index: 2;
+
+
+  &:hover {
+    opacity: 1;
+    z-index: 4;
+  }
+
+  &:hover > * {
+    transform: translateY(0);
+  }
+
+  &: > * {
+    transform: translateY(20px);
+    transition: transform 0.25s;
+  }
+
+
 
   @media only screen and (min-width: 100px) and (max-width: 320px) {
 
@@ -198,324 +252,88 @@ const Slide = styled.div`
 `;
 
 
+const ImageTitle = styled.div`
+
+  font-size: 2em;
+  font-weight:bold;
+
+
+
+  
+  @media only screen and (min-width: 100px) and (max-width: 320px) {
+
+  }
+  @media only screen and (min-width: 321px) and (max-width: 600px) {
+   
+  }
+  @media only screen and (min-width: 601px) and (max-width: 1024px) {
+  }
+`;
+
+const ImageDescription = styled.div`
+
+  font-size: 1.25em;
+  width: 250px;
+  margin-top: 0.25em;
+  text-align: center;
+  @media only screen and (min-width: 100px) and (max-width: 320px) {
+
+  }
+  @media only screen and (min-width: 321px) and (max-width: 600px) {
+   
+  }
+  @media only screen and (min-width: 601px) and (max-width: 1024px) {
+  }
+`;
 
 
 const Image = styled.img`
-  width: 100%;
-  transition: transform 1s;
+z-index: 1;
+width: 100%;
+height: 100%;
+`
+
+const ImageLink = styled.a`
+width: 100%;
+text-align:center;
+margin-top: 40px; 
+padding: 0.6rem;
+  font-size: 12px;
+  font-weight: 900;
+  color: #fff;
+  background-color: #00b1ff;
+  border: 2px solid #00b1ff;
+  cursor: pointer;
+  width: 30%;
+  transition: 0.25s ease;
+  border-radius: 5%;
+  text-decoration: none;
+  z-index: 1;
 
   &:hover {
-    transform: translateZ(20px);
+    box-shadow: 0 6px 18px 0 rgba(0, 0, 0, 0.1);
+    color: #444444;
+    border-color: transparent;
+    background-color: transparent;
   }
+  @media only screen and (min-width: 100px) and (max-width: 320px) {
+    width: 60%;
+    font-size: 12px;
+  }
+  @media only screen and (min-width: 321px) and (max-width: 600px) {
+    width: 60%;
+    font-size: 14px;
+  }
+  @media only screen and (min-width: 601px) and (max-width: 1024px) {
+    width: 45%;
+    font-size: 16px;
+  }
+
 `
+
+
+
+
 
 export default Projects
 
-
-
-
-
-
-
-/*
-linear-gradient(to right, rgba(208,207,205,1) 0%, rgba(255,255,255,0) 100%);
-
-
-
-const useStyles = makeStyles({
-  card: {
-    maxWidth: 300,
-    height: '410px',
-    marginRight: 'auto',
-    marginLeft: 'auto',
-    borderRadius: '9px',
-    boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
-    '&:hover': {
-      backgroundColor: 'transparent',
-      color: '#747474',
-      boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(231,176,255,1)'
-    },
-    ['@media (max-width:600px)']: {
-      height: '320px',
-      width: '250px'
-    },
-    ['@media only screen and (min-width: 601px) and (max-width: 1024px)']: {
-      height: '375px'
-    },
-    '& h2': {
-      textAlign: 'center',
-      fontSize: '25px',
-      fontWeight: 'bold'
-    },
-    '& p': {
-      fontSize: 'calc(14px + (17 - 14) * ((100vw - 300px) / (1600 - 300)))',
-      textAlign: 'center'
-    },
-    '& p:hover': {
-      color: 'black'
-    }
-  },
-  button: {
-    fontSize: '16px',
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    verticalAlign: 'middle',
-    '& a': {
-      textDecoration: 'none',
-      color: '#267ee2'
-    },
-    ['@media only screen and (min-width: 601px) and (max-width: 1024px)']: {
-      fontSize: '12px'
-    }
-  },
-  root: {
-    flexGrow: 1
-  }
-})
-
-
------------------------------ render
- <Container
-      maxWidth="lg"
-      style={{
-        marginTop: '12vh',
-        marginBottom: '5vh'
-      }}
-    >
-      <Header2>Moje projekty</Header2>
-      <div
-        style={{
-          position: 'relative',
-          display: 'inline-block',
-          width: '100%'
-        }}
-      >
-        <BgImg3 src={img1} alt="background" />
-        <Div>
-          <Grid container spacing={2} id="cards">
-            <Grid item xs>
-              <Card className={classes.card}>
-                <CardMedia
-                  component="img"
-                  alt="MgrGracz"
-                  style={{
-                    width: '50%',
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                    marginTop: '1rem'
-                  }}
-                  image={moonFlappyBird1}
-                  title="Pomóż mi osiągnąć najlepszy floor"
-                />
-                <CardContent>
-                <Typography gutterBottom variant="h2" component="h2">
-                    Moon Flappy Bird
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                  Unity3D
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                  Klon Flappy Bird w świecie krypto.
-                  </Typography>
-                </CardContent>
-
-                <CardActions>
-                  <Button
-                    size="small"
-                    color="primary"
-                    className={classes.button}
-                  >
-                    <a
-                      href="https://play.unity.com/mg/other/moonflappybird"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                     Zagraj
-                    </a>
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-            <Grid item xs>
-              <Card className={classes.card}>
-                <CardMedia
-                  style={{
-                    width: '40%',
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                    marginTop: '1rem'
-                  }}
-                  component="img"
-                  alt="MgrGracz"
-                  image={mugOfWar}
-                  title="Gra stworzona w zespole 5 osobowym."
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h2" component="h2">
-                    Mug of war
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                  Unity3D
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    Projekt gry realizowany na GameJamie 
-                    PGG JAM: All Play Accessibility Vol3
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button
-                    size="small"
-                    color="primary"
-                    className={classes.button}
-                  >
-                    <a
-                      href="https://wolix.itch.io/mug-of-war"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Pobierz
-                    </a>
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-            <Grid item xs>
-              <Card className={classes.card}>
-                <CardMedia
-                  component="img"
-                  alt="RPG"
-                  style={{
-                    width: '60%',
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                    marginTop: '1rem'
-                  }}
-                  image={rpg1}
-                  title="Gra stworzona w Gamer Maker Studio"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h2" component="h2">
-                    Praca inżynierska 
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                  Game maker studio
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                  Gra utworzona jako projekt pracy inżynierskiej, projekt RPG w stylu 2d pokemon.
-                  </Typography>
-                </CardContent>
-
-                <CardActions>
-                  <Button
-                    size="small"
-                    color="primary"
-                    className={classes.button}
-                  >
-                    <a
-                      href="https://drive.google.com/file/d/0B3YezdW3G2_wdlh3MHFoME02YVk/view?resourcekey=0-awLMbAwoLZ5KVTgnAbogpQ"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Pobierz
-                    </a>
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-            <Grid item xs>
-              <Card className={classes.card}>
-                <CardMedia
-                  component="img"
-                  alt="Kolektyw NFT"
-                  style={{
-                    width: '60%',
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                    marginTop: '1rem'
-                  }}
-                  image={kolektyw}
-                  title="NFT POLSKA - to świetna Polska społeczność"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h2" component="h2">
-                   Konkurs "Zdefiniuj kolektyw"
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                Unity3d
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                Wizja metaversu i kolektywu w nim umieszczonego.
-                  </Typography>
-                </CardContent>
-
-                <CardActions>
-                  <Button
-                    size="small"
-                    color="primary"
-                    className={classes.button}
-                  >
-                    <a
-                      href="https://drive.google.com/drive/u/0/folders/1-2yB60eHijGYJa4U7gMTdoAVsDfNIs2S"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Pobierz
-                    </a>
-                    
-                  </Button>
-                  <Button
-                    size="small"
-                    color="primary"
-                    className={classes.button}
-                  >
-                    <a
-                      href="https://twitter.com/MgrGracz/status/1565102856730038272?s=20&t=03TLQ8-nqShxZlwY-d4BSw"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Twitter
-                    </a>
-                    
-                  </Button>
-                </CardActions>
-              </Card>
-            </Grid>
-          </Grid>
-        </Div>
-      </div>
-    </Container>
-
-
-*/
